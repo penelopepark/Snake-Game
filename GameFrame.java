309e5728b7847e7cb4ce97818ae964bdc2497f06
Penelope <penelope.park@mail.utoronto.ca>
2024-11-29 14:16:08 -0500

    Implemented GameFrame to configure the main game window.

--- GameFrame.java
+++ GameFrame.java
@@ -1,2 +1,12 @@
-public class GameFrame {
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+    public GameFrame(WeatherService weatherService, HighScoreManager highScoreManager) {
+        setTitle("Snake Game!");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        add(new GamePanel(weatherService, highScoreManager));
+        pack();
+        setLocationRelativeTo(null);
+    }
 }
