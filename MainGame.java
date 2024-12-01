b371dd0bb61575abc3fa5757b670589e7dd4b88b
Penelope <penelope.park@mail.utoronto.ca>
2024-12-01 00:10:48 -0500

    Set up MainGame to initialize services and integrate with GameFrame.

--- MainGame.java
+++ MainGame.java
@@ -1,5 +1,12 @@
+import javax.swing.SwingUtilities;
+
 public class MainGame {
     public static void main(String[] args) {
-
+        SwingUtilities.invokeLater(() -> {
+            WeatherService weatherService = new OpenWeatherMapService("0b4fbd9381a0f4dee2472a7e9783c5a2", "Antarctica");
+            HighScoreManager highScoreManager = HighScoreManager.getInstance();
+            GameFrame gameFrame = new GameFrame(weatherService, highScoreManager);
+            gameFrame.setVisible(true);
+        });
     }
 }
