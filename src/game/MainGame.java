b49bbb8e52e69943a1db2ea559bf12a521ef640f
Penelope <penelope.park@mail.utoronto.ca>
2024-12-02 22:43:32 -0500

    Tested a new location to see if weather changes.

--- src/game/MainGame.java
+++ src/game/MainGame.java
@@ -7,7 +7,7 @@ import javax.swing.SwingUtilities;
 public class MainGame {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            WeatherService weatherService = new OpenWeatherMapService("0b4fbd9381a0f4dee2472a7e9783c5a2", "Antarctica");
+            WeatherService weatherService = new OpenWeatherMapService("0b4fbd9381a0f4dee2472a7e9783c5a2", "Las Vegas, Nevada");
             HighScoreManager highScoreManager = HighScoreManager.getInstance();
             GameFrame gameFrame = new GameFrame(weatherService, highScoreManager);
             gameFrame.setVisible(true);
