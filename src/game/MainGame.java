7801979c09ace0f5aafcfa763626bd9c88d9962b
Penelope <penelope.park@mail.utoronto.ca>
2024-12-03 00:50:37 -0500

    Tested a new location.

--- src/game/MainGame.java
+++ src/game/MainGame.java
@@ -7,7 +7,7 @@ import javax.swing.SwingUtilities;
 public class MainGame {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            WeatherService weatherService = new OpenWeatherMapService("0b4fbd9381a0f4dee2472a7e9783c5a2", "Las Vegas, Nevada");
+            WeatherService weatherService = new OpenWeatherMapService("0b4fbd9381a0f4dee2472a7e9783c5a2", "Tanzania");
             HighScoreManager highScoreManager = HighScoreManager.getInstance();
             GameFrame gameFrame = new GameFrame(weatherService, highScoreManager);
             gameFrame.setVisible(true);
